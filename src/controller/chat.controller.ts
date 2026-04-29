8254d012d95d4926db53c9298bf325628bccf226
harshsimplifygenai <harsh@simplifygenai.com>
2026-04-29 12:00:36 +0530

    log

--- src/controller/chat.controller.ts
+++ src/controller/chat.controller.ts
@@ -9,6 +9,7 @@ import { AgentName, RETELL_AGENT_IDS } from '../constants';
 import logger from '../utils/logger';
 
 export const handleListAgents = (_req: Request, res: Response) => {
+  console.log(RETELL_AGENT_IDS);
   return res.status(200).json({ agents: Object.keys(RETELL_AGENT_IDS) });
 };
 
